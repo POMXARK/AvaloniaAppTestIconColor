34dc4a9c0db5665dc2ebf7ef12a06c3f10629a29
POMXARK <1997pom@gmail.com>
2022-12-22 16:27:54 +1000

    Add project files.

---
+++ AvaloniaAppTestIconColor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaloniaAppTestIconColor.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ AvaloniaAppTestIconColor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaAppTestIconColor.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
